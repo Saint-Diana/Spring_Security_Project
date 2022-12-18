6ef2349cca501c5c36ff37fc39e4600383e23eab
ShenHuiChang <85936940+Saint-Diana@users.noreply.github.com>
2022-12-18 19:35:17 +0800

    添加权限authorities属性

--- TokenDemo/src/main/java/com/shen/entity/LoginUser.java
+++ TokenDemo/src/main/java/com/shen/entity/LoginUser.java
@@ -1,12 +1,17 @@
 package com.shen.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 自定义UserDetails接口实现类
@@ -21,9 +26,22 @@ import java.util.Collection;
 public class LoginUser implements UserDetails {
     private User user;
 
+    private List<String> permissions;
+
+    @JSONField(serialize = false)       //这个成员变量不会序列化保存到redis中
+    private List<SimpleGrantedAuthority> authorities;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        //只需要封装一次，节约时间
+        if(!Objects.isNull(authorities)){
+            return authorities;
+        }
+        //把permissions中String类型的权限信息封装成GrantedAuthority的实现类SimpleGrantedAuthority的对象
+        authorities = permissions.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return authorities;
     }
 
     @Override
