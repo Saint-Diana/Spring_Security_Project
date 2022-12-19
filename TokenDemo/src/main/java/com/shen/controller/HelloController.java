ced3a417eb37d32e33cf662de491462a3b35b791
ShenHuiChang <85936940+Saint-Diana@users.noreply.github.com>
2022-12-19 11:13:56 +0800

    自定义权限验证方法

--- TokenDemo/src/main/java/com/shen/controller/HelloController.java
+++ TokenDemo/src/main/java/com/shen/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    @PreAuthorize("hasAuthority('system:main:upload')")   //只有拥有"system:main:upload"权限的用户才可以访问该请求
+    @PreAuthorize("@myExpressionRoot.hasAuthority('system:main:upload')")   //只有拥有"system:main:upload"权限的用户才可以访问该请求
     public String hello(){
         return "hello";
     }
---
+++ TokenDemo/src/main/java/com/shen/expression/MyExpressionRoot.java
@@ -0,0 +1,26 @@
+package com.shen.expression;
+
+import com.shen.entity.LoginUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 自定义权限校验方法
+ *
+ * @author Shen
+ * @date 2022/12/19 10:50
+ */
+@Component
+public class MyExpressionRoot {
+    public boolean hasAuthority(String authority){
+        //获取当前用户的权限
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        List<String> permissions = loginUser.getPermissions();
+        //判断用户权限集合中是否存在authority
+        return permissions.contains(authority);
+    }
+}
